4d0e9d116f5aae2729c1960d852869100ddc5ee4
Yar <slvwinswim@gmail.com>
2023-08-31 21:23:10 +0300

    bug fixed

--- src/services/marvelService.jsx
+++ src/services/marvelService.jsx
@@ -29,7 +29,7 @@ class marvelService {
     _transformCharacter = (char) => {
         return {
             name: char.name,
-            description: char.description,
+            description: char.description ? char.description.length > 230 ? char.description.slice(0, 230) + '...' : char.description : 'No description',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url
