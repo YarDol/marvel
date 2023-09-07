8e04a0ce5b41ecff943f126bae0f2a5046127d7c
Yar <slvwinswim@gmail.com>
2023-09-07 11:40:00 +0300

    active character

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -69,8 +69,20 @@ class CharList extends Component {
         }
       };
 
+    itemsRef = [];
+
+    setRef = (ref) => {
+        this.itemsRef.push(ref);
+    }
+
+    onFocus = (id) => {
+        this.itemsRef.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemsRef[id].classList.add('char__item_selected');
+        this.itemsRef[id].focus();
+    }
+
     renderCharacters(arr){
-        const items =  arr.map((item) => {
+        const items =  arr.map((item, i) => {
             let imgStyle = {'objectFit' : 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = {'objectFit' : 'unset'};
@@ -79,8 +91,20 @@ class CharList extends Component {
             return (
                 <li 
                     className="char__item"
+                    tabIndex={0}
+                    ref={this.setRef}
                     key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}>
+                    onClick={() => {
+                        this.props.onCharSelected(item.id)
+                        this.onFocus(i);
+                    }}
+                    onKeyPress={(e) => {
+                        if(e.key === ' ' || e.key === "Enter"){
+                            e.preventDefault();
+                            this.props.onCharSelected(item.id);
+                            this.onFocus(i);
+                        }
+                    }}>
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                 </li>
