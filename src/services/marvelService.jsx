7ad00baa8dda89aaf130ae4c1492851253e64301
Yar <slvwinswim@gmail.com>
2023-09-18 15:10:57 +0300

    Servise request update

--- src/services/marvelService.jsx
+++ src/services/marvelService.jsx
@@ -13,6 +13,16 @@ const useMarvelService = () => {
         return _transformCharacter(res.data.results[0]);
     }
 
+    const getAllComics = async (offset = 0) => {
+        const res = await request (`${process.env.REACT_APP_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${process.env.REACT_APP_apiKey}`);
+        return res.data.results.map(_transformComics);
+    }
+
+    const getComics = async (id) => {
+        const res = await request(`${process.env.REACT_APP_apiBase}comics/${id}?${process.env.REACT_APP_apiKey}`);
+        return _transformComics(res.data.results[0]);
+    }
+
     const _transformCharacter = (char) => {
         return {
             id: char.id,
@@ -25,7 +35,23 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, getAllCharacters, getCharacter, clearError}
+    const _transformComics = (comics) => {
+        return {
+            id: comics.id,
+			title: comics.title,
+			description: comics.description || "There is no description",
+			pageCount: comics.pageCount
+				? `${comics.pageCount} p.`
+				: "No information about the number of pages",
+			thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
+			language: comics.textObjects[0]?.language || "en-us",
+			price: comics.prices[0].price
+				? `${comics.prices[0].price}$`
+				: "not available",
+        }
+    }
+
+    return {loading, error, getAllCharacters, getCharacter, getAllComics, getComics, clearError}
 }
 
 export default useMarvelService;
