ed630a3c80fe197bb8f168e3a5ffe33815a7dea9
Yar <slvwinswim@gmail.com>
2023-09-20 14:29:31 +0300

    separate comic's page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,6 +3,7 @@ import AppHeader from "../appHeader/AppHeader";
 import MainPage from "../pages/MainPage";
 import ComicsPage from "../pages/ComicsPage";
 import Page404 from "../pages/404";
+import SingleComicPage from "../pages/singleComicPage";
 
 const App = () => {
 
@@ -14,6 +15,7 @@ const App = () => {
                     <Routes>
                         <Route path="/" element={<MainPage/>}/>
                         <Route path="/comics" element={<ComicsPage/>}/>
+                        <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
                         <Route path="*" element={<Page404/>}/>
                     </Routes>
                 </main>
